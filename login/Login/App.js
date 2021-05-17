a4ce9614dc2be0cac94d96b091b3eafc50d6a5e6
tiendat19521345 <71374867+tiendat19521345@users.noreply.github.com>
2021-05-17 10:24:38 +0700

    Nè Linh oi

---
+++ login/Login/App.js
@@ -0,0 +1,14 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+
+
+import Login from './screens/Login';
+import Signup from './screens/Signup';
+
+
+
+export default function App() {
+  return <Login />;
+}
+
+
